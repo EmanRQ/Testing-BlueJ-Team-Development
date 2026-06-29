2049126ef6778d64a38462c8d659cf0adddc3d43
EmanRQ <emanrifqie@gmail.com>
2026-06-29 22:01:09 +0800

    Initial sharing of project

---
+++ NEXA_Team.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class NEXA_Team here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NEXA_Team
+{
+    protected String name;
+    protected String role;
+}
