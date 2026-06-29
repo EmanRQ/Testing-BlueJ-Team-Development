0567a48777f231596800c8d30e6761a81a7448e3
EmanRQ <emanrifqie@gmail.com>
2026-06-29 22:22:16 +0800

    Make a complete main. also make an array with txt file to stored data the main now can check the roles based on team members name

---
+++ NEXA_Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+public class NEXA_Main 
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        NEXA_Team[] members = new NEXA_Team[4];
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Members.txt"));
+            for (int i = 0; i < 4; i++) {
+                String line = br.readLine();
+                if (line != null) {
+                    String[] parts = line.split(", ");
+                    members[i] = new NEXA_Team(parts[0], parts[1]);
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+        System.out.println("Hello Visitor ^ ^ ");
+        System.out.println("               >  ");
+        System.out.print("\nAre you NEXA members? [Y/N]: ");
+        char answer = in.next().toUpperCase().charAt(0);
+        in.nextLine();
+
+        if(answer == 'Y') {
+            System.out.print("Please Enter Your Name: ");
+            String inputName = in.nextLine();
+            boolean found = false;
+
+            for (NEXA_Team m : members) {
+                if (m != null && m.getName().equalsIgnoreCase(inputName)) {
+                    System.out.println("Welcome " + m.getName() + "!! " + "\nYour role is: " + m.getRole());
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) System.out.println("Member not found in database.");
+        } else {
+            System.out.println("Get Out!!!");
+        }
+        in.close();
+    }
+}
--- NEXA_Team.java
+++ NEXA_Team.java
@@ -1,12 +1,16 @@
-
-/**
- * Write a description of class NEXA_Team here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class NEXA_Team
-{
+public class NEXA_Team {
     protected String name;
     protected String role;
+
+    public NEXA_Team(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getRole() {
+        return role;
+    }
 }
